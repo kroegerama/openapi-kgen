76780abfd461954c54128c3848358abbc563cb9e
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-09-19 22:41:52 +0200

    fix sonatype config

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ subprojects {
         repositories {
             maven {
                 name = "sonatype"
-                setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
                 credentials {
                     username = nexusUsername
@@ -102,6 +102,9 @@ nexusPublishing {
             stagingProfileId.set(nexusStagingProfileId)
             username.set(nexusUsername)
             password.set(nexusPassword)
+
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
