9cea686a57d8cc7a8fb8c20b59dbe13f801f1c07
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-10-14 11:48:04 +0200

    * increase project version * downgrade swagger parser -> newer version had bugs

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object C {
     const val PROJECT_NAME = "OpenAPI KGen"
     const val PROJECT_DESCRIPTION = "Generate modern API Clients in Kotlin from OpenAPI specifications. Supports OpenAPI >= 3.0.0."
 
-    const val PROJECT_VERSION = "0.12.3"
+    const val PROJECT_VERSION = "0.12.4"
     const val PROJECT_GROUP_ID = "com.kroegerama.openapi-kgen"
 
     const val PROJECT_URL = "https://github.com/kroegerama/openapi-kgen"
@@ -17,7 +17,7 @@ object V {
     const val AIRLINE = "0.9"
     const val GUAVA = "29.0-jre"
     const val ICU = "67.1"
-    const val SWAGGER_PARSER = "2.0.24"
+    const val SWAGGER_PARSER = "2.0.20"
     const val KOTLIN_POET = "1.9.0"
 }
 
