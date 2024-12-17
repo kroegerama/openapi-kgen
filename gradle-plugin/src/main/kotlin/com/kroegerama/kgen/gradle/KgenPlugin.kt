7dafbe3e62f97616d24be82e599752437a637675
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-12-17 17:40:23 +0100

    remove `useKsp` (ksp is always used)

--- gradle-plugin/src/main/kotlin/com/kroegerama/kgen/gradle/KgenPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/kroegerama/kgen/gradle/KgenPlugin.kt
@@ -4,15 +4,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.tasks.TaskProvider
-import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.register
-import org.gradle.kotlin.dsl.withType
+import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.ide.idea.model.IdeaModel
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
-import org.jetbrains.kotlin.gradle.internal.KaptTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.File
 
@@ -21,11 +15,7 @@ class KgenPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
         val kgenExtension = extensions.create<KgenExtension>("kgen")
 
-        if (kgenExtension.useKsp) {
-            pluginManager.apply("com.google.devtools.ksp")
-        } else {
-            pluginManager.apply("kotlin-kapt")
-        }
+        pluginManager.apply("com.google.devtools.ksp")
 
         finishEvaluate(kgenExtension)
     }
@@ -58,19 +48,13 @@ class KgenPlugin : Plugin<Project> {
         tasks.withType<KotlinCompile>().configureEach {
             dependsOn(kgenTaskProvider)
         }
-        tasks.withType<KaptTask>().configureEach {
-            dependsOn(kgenTaskProvider)
-            mustRunAfter(kgenTaskProvider)
-        }
     }
 
     private fun Project.addDependencies(kgenExtension: KgenExtension) = dependencies {
-        val processor = if (kgenExtension.useKsp) "ksp" else "kapt"
-
         val moshi = "1.15.1"
         add("implementation", "com.squareup.moshi:moshi:$moshi")
         add("implementation", "com.squareup.moshi:moshi-adapters:$moshi")
-        add(processor, "com.squareup.moshi:moshi-kotlin-codegen:$moshi")
+        add("ksp", "com.squareup.moshi:moshi-kotlin-codegen:$moshi")
 
         val okhttp = "4.12.0"
         add("implementation", "com.squareup.okhttp3:okhttp:$okhttp")
@@ -80,9 +64,9 @@ class KgenPlugin : Plugin<Project> {
         add("implementation", "com.squareup.retrofit2:converter-moshi:$retrofit")
         add("implementation", "com.squareup.retrofit2:converter-scalars:$retrofit")
 
-        val moshiSealed = "0.27.2"
+        val moshiSealed = "0.28.0"
         add("implementation", "dev.zacsweers.moshix:moshi-sealed-runtime:$moshiSealed")
-        add(processor, "dev.zacsweers.moshix:moshi-sealed-codegen:$moshiSealed")
+        add("ksp", "dev.zacsweers.moshix:moshi-sealed-codegen:$moshiSealed")
     }
 
     private inline fun <reified T> ExtensionContainer.findByNameTyped(name: String): T? = findByName(name) as? T
