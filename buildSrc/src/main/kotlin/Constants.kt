cb303b56a90d6f36a6714241db99a2308f0445db
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-12-17 17:41:41 +0100

    update libraries / increase version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object C {
     const val PROJECT_NAME = "OpenAPI KGen"
     const val PROJECT_DESCRIPTION = "Generate modern API Clients in Kotlin from OpenAPI specifications. Supports OpenAPI >= 3.0.0."
 
-    const val PROJECT_VERSION = "0.16.0"
+    const val PROJECT_VERSION = "0.17.0"
     const val PROJECT_GROUP_ID = "com.kroegerama.openapi-kgen"
 
     const val PROJECT_URL = "https://github.com/kroegerama/openapi-kgen"
@@ -12,12 +12,12 @@ object C {
 
 object V {
     const val KOTLIN = "1.9.24"
-    const val GRADLE_PUBLISH = "1.2.1"
+    const val GRADLE_PUBLISH = "1.3.0"
     const val AIRLINE = "0.9"
-    const val GUAVA = "33.2.1-jre"
+    const val GUAVA = "33.3.1-jre"
     const val ICU = "75.1"
     const val SWAGGER_PARSER = "2.0.20"
-    const val KOTLIN_POET = "1.18.0"
+    const val KOTLIN_POET = "1.18.1"
     const val SHADOW = "8.1.1"
     const val NEXUS_PUBLISH = "2.0.0"
 }
